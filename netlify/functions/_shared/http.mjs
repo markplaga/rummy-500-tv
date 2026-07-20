8d5c0c82114472e8ad7acf8ecca2856bc0ccd352
Mark Plaga <mark@markplaga.com>
2026-07-20 06:22:48 -0700

    Add server utilities

---
+++ netlify/functions/_shared/http.mjs
@@ -0,0 +1,17 @@
+export function json(data, status = 200) {
+  return new Response(JSON.stringify(data), {
+    status,
+    headers: {
+      'content-type': 'application/json; charset=utf-8',
+      'cache-control': 'no-store'
+    }
+  });
+}
+
+export async function bodyJson(req) {
+  try {
+    return await req.json();
+  } catch {
+    return {};
+  }
+}
