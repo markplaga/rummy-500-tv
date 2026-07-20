10a5b70f4328f15c2c5cf4ba42e361e44440c5dd
Mark Plaga <mark@markplaga.com>
2026-07-20 06:29:31 -0700

    Add QR code helper

---
+++ src/vendor/qrcode.js
@@ -0,0 +1,3 @@
+// QR Code Generator by Kazuhiko Arase, MIT licensed.
+// Loaded from jsDelivr so the application bundle remains small.
+document.write('<script src="https://cdn.jsdelivr.net/npm/qrcode-generator@1.4.4/qrcode.min.js"><\/script>');
